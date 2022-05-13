ad6fe71c500bacc4a0c0271fa01e0889a44a1bea
neko385 <102905708+neko385@users.noreply.github.com>
2022-05-13 15:24:52 +0000

    [Add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!, except: [:top, :about]
     before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sign_in_path_for(resource)
+        user_path(current_user.id)
+    end
 
     def after_sign_out_path_for(resource)
         root_path
@@ -7,7 +12,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
-        devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        
     end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,20 +2,31 @@ class BooksController < ApplicationController
   def index
     @book = Book.new
     @books = Book.all
+    @user = @books.all
+    @user = current_user
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/home/about'
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(current_user.id)
   end
 
   def show
     @book = Book.find(params[:id])
+    @books = Book.all
+    @user = current_user
   end
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
@@ -29,6 +40,8 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title,:body)
   end
 
-
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
 
 end
